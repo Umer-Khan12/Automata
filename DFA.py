91dd2e1e94a0d3a846043b06a548caf203ba2778
Umer Khan <umk499@mail.usask.ca>
2020-04-29 12:59:56 -0600

    Finished DFA implementation.

--- DFA.py
+++ DFA.py
@@ -7,10 +7,8 @@ import DFAState as state
 # States created from DFAState.create()
 q0 = state.create("q0")
 q1 = state.create("q1", True)
-# The input alphabet
-alphabet = ["0", "1"]
-# The initial state
-initialState = "q0"
+# The current state (Starts at initial state)
+currentState = q0
 # The set of transitions implemented as a dictionairy with
 # the key being a tuple (state, letter) and the value being the next state
 # if there is no next state then we will represent it by None
@@ -20,3 +18,20 @@ transitions = {(q0, "0"):q1,
                (q1, "1"):q1
                }
 
+# The word we will run through the DFA
+word = "0111001010"
+
+# Use the DFA to check the word
+for letter in word:
+    # If there is no transition on a current letter then the machine crashes
+    if transitions[(currentState, letter)] is None:
+        break
+    # Else switch to the new state
+    else:
+        currentState = transitions[(currentState, letter)]
+
+# If it ends in a final state then accept, otherwise reject
+if state.isFinal(currentState):
+    print("The word: " + word + " was accepted")
+else:
+    print("The word: " + word + " was rejected")
